cdaeb47b47acf37e1c9bc12b7308d7ada2253e8e
johnsqliu <johnsqliu@tencent.com>
2022-03-31 17:44:52 +0800

    封装axios通用接口请求模块

---
+++ src/api/request.ts
@@ -0,0 +1,70 @@
+import axios, { AxiosInstance, AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
+
+interface MyAxiosInstance extends AxiosInstance {
+  (config: AxiosRequestConfig): Promise<any>;
+  (url: string, config?: AxiosRequestConfig): Promise<any>;
+}
+
+export class Request {
+  public static axiosInstance: MyAxiosInstance;
+
+  public static init() {
+    this.axiosInstance = axios.create({
+      baseURL: '/api',
+      timeout: 10000,
+    });
+
+    this.initInterceptors();
+  }
+
+  public static initInterceptors() {
+    this.axiosInstance.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+
+    this.axiosInstance.interceptors.request.use((config: AxiosRequestConfig) => {
+      const token = localStorage.getItem('ACCESS_TOKEN');
+      if (token) {
+        config.headers!.Authorization = `Bearer ${token}`;
+      }
+      return config;
+    }, (error: AxiosError) => {
+      return Promise.reject(error);
+    });
+
+    this.axiosInstance.interceptors.response.use(
+      (response: AxiosResponse) => {
+        const { data: { code, message, data } } = response;
+        if (response.status !== 200 || code !== 0) {
+          Request.errorHandle(response, message);
+        }
+        return data;
+      },
+      // 请求失败
+      (error: AxiosError): Promise<any> => {
+        const { response } = error;
+        if (response) {
+          // 请求已发出，但是不在2xx的范围
+          Request.errorHandle(response);
+        } else {
+          console.log('网络连接异常,请稍后再试!');
+        }
+        return Promise.reject(response?.data);
+      }
+    );
+  }
+
+  private static errorHandle(res: AxiosResponse, message?: string) {
+    // 状态码判断
+    switch (res.status) {
+      case 401:
+        break;
+      case 403:
+        break;
+      case 404:
+        console.log();('请求的资源不存在');
+        break;
+      default:
+        // 错误信息判断
+        message && console.log(message);
+    }
+  }
+}
